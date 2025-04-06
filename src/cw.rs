9c5e29e654b8f40863a4eeaf33df45a62bc8d6b9
Paul Colusso <paulcolusso@gmail.com>
2025-04-06 21:02:38 +1000

    get logs

---
+++ src/cw.rs
@@ -0,0 +1,41 @@
+use aws_sdk_cloudwatchlogs as cwl;
+
+pub struct Logs {
+    client: cwl::Client,
+    group: String,
+    query: Option<String>,
+    next_token: Option<String>,
+}
+
+impl Logs {
+    pub async fn new(group: String) -> Self {
+        let config = aws_config::load_from_env().await;
+        let client = cwl::Client::new(&config);
+        let query = None;
+        Self { client, group, query, next_token: None }
+    }
+
+    pub fn set_query(&mut self, query: String) {
+        self.query = Some(query);
+        self.next_token = None;
+    }
+
+    // idk, should we handle back & forward? For now, assume ALWAYS FORWARD
+    pub async fn get_more_logs(&mut self) -> Vec<(i64, String)> {
+        let req = self.client.filter_log_events()
+            .log_group_name(&self.group)
+            .limit(40)
+            .set_filter_pattern(self.query.clone())
+            .set_next_token(self.next_token.clone());
+        let res = req.send().await.unwrap();
+        self.next_token = res.next_token().map(|s| s.into());
+        // What's more correct, returning an empty vec, or an option and acually dealing with it...
+        let mut evts = Vec::new();
+        for event in res.events() {
+            if let (Some(x), Some(y)) = (event.timestamp(), event.message()) {
+                evts.push((x, y.to_owned()));
+            }
+        }
+        evts
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
 use anyhow::Result;
 use clap::Parser;
-use aws_sdk_cloudwatch as cw;
 
 mod ui;
+mod cw;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Config {
-    log_group_name: String
+    log_group_name: String,
+    #[arg(short, long, default_value = "84")]
+    snip: Option<usize>
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let config = aws_config::load_from_env().await;
-    let client = aws_sdk_cloudwatch::Client::new(&config);
     let config = Config::parse();
 
-    ui::run().await?;
+    let mut cw = cw::Logs::new(config.log_group_name).await;
+    for (x, y) in cw.get_more_logs().await {
+        let msg = if let Some(snip) = config.snip {
+            y.chars().skip(snip).collect::<String>()
+        } else {
+            y
+        };
+        println!("{}: {}", x, msg);
+    }
+
+    //ui::run().await?;
 
     Ok(())
 }
