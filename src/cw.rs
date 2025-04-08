eab00b2e1c216326a93a837c308fb7451323249b
Paul Colusso <paulcolusso@gmail.com>
2025-04-08 12:01:01 +1000

    what a mess lmao

--- src/cw.rs
+++ src/cw.rs
@@ -6,6 +6,8 @@ pub struct Logs {
     group: String,
     query: Option<String>,
     next_token: Option<String>,
+    // kludge, we need to identify which stream we need.
+    stream: Option<String>,
 }
 
 // This looks a lot like an iterator, or a cursor...
@@ -14,7 +16,17 @@ impl Logs {
         let config = aws_config::load_from_env().await;
         let client = cwl::Client::new(&config);
         let query = None;
-        Self { client, group, query, next_token: None }
+        Self { client, group, query, next_token: None, stream: None }
+    }
+
+    async fn get_first_stream(&self) -> Option<String> {
+        let req = self.client.describe_log_streams()
+            .order_by(aws_sdk_cloudwatchlogs::types::OrderBy::LastEventTime)
+            .descending(true)
+            .log_group_name(&self.group)
+            .limit(1);
+        let res = req.send().await.unwrap();
+        res.log_streams()[0].log_stream_name().map(|s| s.to_owned())
     }
 
     pub fn set_query(&mut self, query: String) {
@@ -27,10 +39,10 @@ impl Logs {
         let end_time = middle + 1000;
         let req = self.client.get_log_events()
             .log_group_name(&self.group)
+            .log_stream_name(self.stream.clone().expect("Stream not set"))
             .start_time(start_time)
             .end_time(end_time)
-            .limit(40)
-            .set_next_token(self.next_token.clone());
+            .limit(40);
         let res = req.send().await.unwrap();
         let mut evts = Vec::new();
         for event in res.events() {
@@ -43,8 +55,10 @@ impl Logs {
 
     // idk, should we handle back & forward? For now, assume ALWAYS FORWARD
     pub async fn get_more_logs(&mut self) -> Vec<(i64, String)> {
+        self.stream = self.get_first_stream().await;
         let req = self.client.filter_log_events()
             .log_group_name(&self.group)
+            .set_log_stream_names(Some(vec![self.stream.clone().unwrap()]))
             .limit(40)
             .set_filter_pattern(self.query.clone())
             .set_next_token(self.next_token.clone());
--- src/ui.rs
+++ src/ui.rs
@@ -1,14 +1,16 @@
-use std::{future::Future, time::Duration};
+use std::{future::Future, sync::{Arc}, time::Duration};
 
 use crossterm::event::{Event, EventStream, KeyCode, KeyEvent, KeyEventKind};
 use futures::{FutureExt, StreamExt};
 use ratatui::{
-    layout::{Constraint, Direction, Layout, Rect}, style::{Color, Style, Stylize}, widgets::{Block, Borders, List, ListItem, ListState, Paragraph}, DefaultTerminal, Frame
+    layout::{Constraint, Direction, Flex, Layout, Rect}, style::{Color, Style, Stylize}, widgets::{Block, Borders, Clear, List, ListItem, ListState, Paragraph}, DefaultTerminal, Frame
 };
 use anyhow::{Error, Result};
-use tokio::{sync::mpsc::{channel, Receiver, Sender}, task::JoinHandle, time::sleep};
+use tokio::{sync::{mpsc::{channel, Receiver, Sender}, Mutex}, task::JoinHandle, time::sleep};
 
 use crate::cw::Logs;
+
+type Z<T> = Arc<tokio::sync::Mutex<T>>;
 
 pub async fn run(logs: Logs) -> Result<()> {
     let terminal = ratatui::init();
@@ -30,26 +32,18 @@ impl<T> From<(Sender<T>, Receiver<T>)> for Chan<T> {
     }
 }
 
-const fn alternate_colors(i: usize) -> Color {
-    if i % 2 == 0 {
-        Color::DarkGray
-    } else {
-        Color::Black
-    }
-}
-
 enum Modal {
     Hidden,
     Visible(i64)
 }
 
-#[derive()]
+// Made a proper mess of this state... Some enums could probably better cover the load states.
 pub struct App {
     // Is the application running?
     // gee i sure hope so
     running: bool,
     event_stream: EventStream,
-    client: Logs,
+    client: Z<Logs>,
     data_chan: Chan<Data>,
     error_chan: Chan<Error>,
     fut: Option<JoinHandle<()>>,
@@ -79,9 +73,6 @@ enum Status {
     Failed(Error),
 }
 
-type LoadFn = Box<dyn FnOnce(Logs, Sender<Data>, Sender<Error>) -> Box<dyn Future<Output = ()> + Send + 'static> + Send + 'static>;
-
-
 impl App {
     /// Construct a new instance of [`App`].
     pub fn new(client: Logs) -> Self {
@@ -95,6 +86,7 @@ impl App {
         let fut = None;
         let modal = Modal::Hidden;
         let ctx_rows = Vec::new();
+        let client = Arc::new(Mutex::new(client));
 
         let mut res = Self { ctx_rows, modal, fut, running, event_stream, client, data_chan, error_chan, status, rows, query };
         res.load_more();
@@ -102,21 +94,30 @@ impl App {
         res
     }
 
-
-
     // Wrap the async function, spawn it and send it's results back via channels.
+    // The sig is nasty, but it's saying we need a func that takes in the client, the channels and returns Promise<void> (teehee)
+    // Tried to simplify it, it would have to be defined as a trait, therefore a "Functor", which adds too much more complexity;
+    // or via dynamic dispatch, but then we summon the pin demons.
     fn load<F, Fut>(&mut self, act: F)
-        where F: FnOnce(Logs, Sender<Data>, Sender<Error>) -> Fut + 'static + Send,
+        where F: FnOnce(Z<Logs>, Sender<Data>, Sender<Error>) -> Fut + 'static + Send,
               Fut: Future<Output = ()> + Send + 'static
     {
         self.status = Status::Loading;
+        // Naive debouncing, we tack on a sleep to all load requests,
+        // subsequent ones get cancelled. This does add some delay, but consideirng every API call
+        // is billed, probably preferable than dispatching every keystroke.
         if let Some(f) = self.fut.take() {
+            // Is this the correct way to cancel a task? Seems ok if we have no "cleanup"
+            // Notably, dropping the JoinHandle does not cancel the task!
             f.abort();
         }
 
+        // Channels are easy and all, but I feel it's a bit messy. idk if the Actor pattern
+        // is worthwhile here...Might be a good exercise if you want to try and "improve" the
+        // code quality lmao.
         let data_tx = self.data_chan.tx.clone();
         let err_tx = self.error_chan.tx.clone();
-        let logs = self.client.clone();
+        let logs = Arc::clone(&self.client);
         self.fut = Some(tokio::spawn(async move {
             sleep(Duration::from_millis(33)).await;
             act(logs, data_tx, err_tx).await;
@@ -125,7 +126,8 @@ impl App {
 
     fn load_more(&mut self) {
         let q = self.query.clone();
-        self.load(|mut logs, data_tx, err_tx| async move  {
+        self.load(|logs, data_tx, err_tx| async move  {
+            let mut logs = logs.lock().await;
             logs.set_query(q);
             let result = logs.get_more_logs().await;
             let wrapped = Data::Logs(result);
@@ -136,7 +138,8 @@ impl App {
     }
 
     fn load_context(&mut self, ts: i64) {
-        self.load(move |mut logs, data_tx, err_tx| async move {
+        self.load(move |logs, data_tx, err_tx| async move {
+            let mut logs = logs.lock().await;
             let result = logs.find_context(ts).await;
             let wrapped = Data::Context(result);
             if let Err(e) = data_tx.send(wrapped).await {
@@ -155,56 +158,47 @@ impl App {
         Ok(())
     }
 
-    fn draw_modal(&self, f: &mut Frame) {
-
-    }
-
     fn draw(&mut self, f: &mut Frame) {
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(1)
-            .constraints(
-                [
-                    Constraint::Fill(1),
-                    Constraint::Length(3),
-                    Constraint::Length(1)
-                ]
-                .as_ref(),
-            )
-            .split(f.area());
-
-        self.render_list(chunks[0], f);
-
         match self.modal {
             Modal::Visible(_) => {
                 self.draw_modal(f);
             }
-            Modal::Hidden => {}
+            Modal::Hidden => {
+                let chunks = Layout::default()
+                    .direction(Direction::Vertical)
+                    .margin(1)
+                    .constraints(
+                        [
+                            Constraint::Fill(1),
+                            Constraint::Length(3),
+                            Constraint::Length(1)
+                        ]
+                        .as_ref(),
+                    )
+                    .split(f.area());
+                self.render_list(chunks[0], f);
+
+                let input_paragraph = Paragraph::new(self.query.clone())
+                    .block(Block::default().borders(Borders::ALL).title("Query"));
+                f.render_widget(input_paragraph, chunks[1]);
+
+                let s = match self.status {
+                    Status::Loaded => " Done".into(),
+                    Status::Loading => " Loading...".into(),
+                    Status::Failed(ref e) => format!(" Error: {}", e),
+                };
+                let status_line = Paragraph::new(s)
+                    .fg(Color::White)
+                    .bg(Color::Blue);
+                f.render_widget(status_line, chunks[2]);
+            }
         }
-
-        let input_paragraph = Paragraph::new(self.query.clone())
-            .block(Block::default().borders(Borders::ALL).title("Query"));
-        f.render_widget(input_paragraph, chunks[1]);
-
-        let s = match self.status {
-            Status::Loaded => " Done".into(),
-            Status::Loading => " Loading...".into(),
-            Status::Failed(ref e) => format!(" Error: {}", e),
-        };
-        let status_line = Paragraph::new(s)
-            .fg(Color::White)
-            .bg(Color::Blue);
-        f.render_widget(status_line, chunks[2]);
     }
 
     fn render_list(&mut self, area: Rect, f: &mut Frame) {
         let list_items: Vec<ListItem> = self.rows.items
             .iter()
-            .enumerate()
-            .map(|(i, item)| {
-                let c = alternate_colors(i);
-                ListItem::from(item.1.clone())
-            })
+            .map(|item| ListItem::from(item.1.clone()))
             .collect();
 
         let list = List::new(list_items)
@@ -215,9 +209,21 @@ impl App {
         f.render_stateful_widget(list, area, &mut self.rows.state);
     }
 
+    fn draw_modal(&self, f: &mut Frame) {
+        let area = popup_area(f.area(), 90, 90);
+        let block = Block::bordered().title("Modal");
+        let list = List::new(self.ctx_rows.iter().map(|row| {
+            ListItem::new(row.to_string())
+        })).block(block);
+        f.render_widget(Clear, area);
+        f.render_widget(list, area);
+    }
 
     /// Reads the crossterm events and updates the state of [`App`].
     async fn handle_crossterm_events(&mut self) -> Result<()> {
+        // 'Fuse' means as in like blowing a fuse.
+        // It means after the Future returns None, do not poll it again.
+        // Streams should be fused, if they are used in a select! macro.
         tokio::select! {
             event = self.event_stream.next().fuse() => {
                 match event {
@@ -234,12 +240,13 @@ impl App {
                     _ => {}
                 }
             }
-            err = self.error_chan.rx.recv() => {
+            // See, the fuse here is probably unnecesssary, as the channel will not close.
+            err = self.error_chan.rx.recv().fuse() => {
                 if let Some(err) = err {
                     self.status = Status::Failed(err);
                 }
             }
-            data = self.data_chan.rx.recv() => {
+            data = self.data_chan.rx.recv().fuse() => {
                 match data {
                     Some(Data::Logs(logs)) => {
                         self.rows.items = logs;
@@ -253,6 +260,8 @@ impl App {
                 }
                 self.status = Status::Loaded;
             }
+            // Disabled, because I don't think we have any ticker based functionality.
+            // So, in theory, absent of input or network activity, we should have almost zero CPU?
             // _ = tokio::time::sleep(tokio::time::Duration::from_millis(100)) => {
             //     // Sleep for a short duration to avoid busy waiting.
             // }
@@ -272,8 +281,17 @@ impl App {
                 }
                 self.quit();
             },
-            (_, KeyCode::Down) => self.rows.state.select_next(),
-            (_, KeyCode::Up) => self.rows.state.select_previous(),
+            (_, KeyCode::Down) => {
+                if matches!(self.modal, Modal::Hidden) {
+                    self.rows.state.select_next();
+                }
+
+            },
+            (_, KeyCode::Up) => {
+                if matches!(self.modal, Modal::Hidden) {
+                    self.rows.state.select_previous();
+                }
+            },
             (_, KeyCode::Char(a)) => {
                 if matches!(self.modal, Modal::Hidden) {
                     self.query.push(a);
@@ -303,4 +321,12 @@ impl App {
     fn quit(&mut self) {
         self.running = false;
     }
+}
+
+fn popup_area(area: Rect, percent_x: u16, percent_y: u16) -> Rect {
+    let vertical = Layout::vertical([Constraint::Percentage(percent_y)]).flex(Flex::Center);
+    let horizontal = Layout::horizontal([Constraint::Percentage(percent_x)]).flex(Flex::Center);
+    let [area] = vertical.areas(area);
+    let [area] = horizontal.areas(area);
+    area
 }
